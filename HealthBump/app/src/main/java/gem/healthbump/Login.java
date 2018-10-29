c41402c314e0e6f5d47533ded2b45cdcea98597e
carlosGem <carlosGem@users.noreply.github.com>
2018-10-29 21:45:21 +0000

    Change Login
    Change to email rather than using username

--- HealthBump/app/src/main/java/gem/healthbump/Login.java
+++ HealthBump/app/src/main/java/gem/healthbump/Login.java
@@ -12,7 +12,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity  implements View.OnClickListener {
 
-    EditText editTextUsername, editTextPassword;
+    EditText editTextEmail, editTextPassword;
     TextView textViewSingUp;
     Button loginBtn;
     FirebaseAuth mAuth;
@@ -22,7 +22,7 @@ public class Login extends AppCompatActivity  implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
 
-        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
         textViewSingUp = (TextView) findViewById(R.id.textViewCreateAccount);
         loginBtn = findViewById(R.id.loginBtn);
@@ -32,7 +32,11 @@ public class Login extends AppCompatActivity  implements View.OnClickListener {
     }
 
     private void userLogin(){
-        mAuth.signInWithEmailAndPassword();
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        mAuth.signInWithEmailAndPassword(email, password){
+
+        }
     }
 
     @Override
